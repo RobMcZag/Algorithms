b57f9fa67364b2c3c15e902e27a1a6f7c462d3ca
Roberto Zagni <rz70rz70@gmail.com>
2013-05-06 18:45:32 +0200

    Working !

--- Algoritmi/src/it/rz/algoritmi/MiddleOfLinkedList.java
+++ Algoritmi/src/it/rz/algoritmi/MiddleOfLinkedList.java
@@ -1,13 +1,58 @@
 package it.rz.algoritmi;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class MiddleOfLinkedList {
+
+	private static final int NUMERO_ELEMENTI = 10;
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {		
+		for (int i = 1; i < NUMERO_ELEMENTI; i++) {
+			// Crea la lista della lunghezza richiesta
+			LinkedList<Object> lista = setupList(i);
 
+			// Trova l'elemento centrale
+			Object mezzo = findMiddle(lista);
+
+			// Stampa risultato
+			System.out.print("Numero elementi: " + i);
+			System.out.println(" - Elemento intermedio: " + mezzo.toString());
+			// System.out.println("");
+
+		}
+		
+	}
+	
+	private static LinkedList<Object> setupList(int n) {
+		LinkedList<Object> lista = new LinkedList<>();
+		for (int i = 1; i <= n; i++) {
+			lista.add(Integer.valueOf(i));
+		}
+		return lista;
 	}
 
+	/**
+	 * Cerca e ritorna l'elemento di mezzo della lista.
+	 * Se la lista non ha elementi ritorna null.
+	 * @param lista
+	 * @return
+	 */
+	public static Object findMiddle(LinkedList<Object> lista) {
+		Object inMezzo = null;
+		int p = 1;
+		Iterator<Object> i = lista.iterator();
+		Iterator<Object> dispari = lista.iterator();
+		
+		while (i.hasNext()) {
+			i.next();
+			if ( (p++ % 2) == 1 ) {  // se p è dispari
+				inMezzo = (Object) dispari.next();;
+			}			
+		}
+		return inMezzo;
+	}
 }
