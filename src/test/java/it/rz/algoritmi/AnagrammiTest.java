88cb453c2471c4df5b976cd19a02704417303803
Roberto Zagni <rz70rz70@gmail.com>
2014-10-06 22:28:08 +0200

    Created Test Class for Anagrammi. <Commit from Eclipse>

---
+++ src/test/java/it/rz/algoritmi/AnagrammiTest.java
@@ -0,0 +1,32 @@
+package it.rz.algoritmi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AnagrammiTest {
+
+	public AnagrammiTest() {
+	}
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testReadWords() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectLong() {
+		fail("Not yet implemented");
+	}
+
+}
