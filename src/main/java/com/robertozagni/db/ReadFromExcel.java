5549ea6134a8e2c36ad354f590d11ec5e47f62ea
Roberto Zagni <rz70rz70@gmail.com>
2016-01-16 23:07:12 +0200

    Renamed class

--- src/main/java/com/robertozagni/db/ReadFromExcel.java
+++ src/main/java/com/robertozagni/db/ReadFromExcel.java
@@ -1,4 +1,4 @@
-package com.robertozagni.algoritmi;
+package com.robertozagni.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DatabaseAccess {
+public class ReadFromExcel {
 
 	/**
 	 * @param args
