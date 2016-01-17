9f5dcc54bf2dc778df58c5117bb8cddb6861ff95
Roberto Zagni <rz70rz70@gmail.com>
2016-01-17 20:00:46 +0200

    Sources have license information.

--- src/main/java/com/robertozagni/algoritmi/list/MiddleOfLinkedList.java
+++ src/main/java/com/robertozagni/algoritmi/list/MiddleOfLinkedList.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2014, 2016 Roberto Zagni, www.robertozagni.com
+ * The contents of this repository  are released under Apache 2.0 License unless stated differently; 
+ * see LICENSE file for complete text. 
+ * 
+ * @author roberto.zagni
+ */
 package com.robertozagni.algoritmi.list;
 
 import java.util.Iterator;
@@ -5,59 +12,60 @@ import java.util.LinkedList;
 
 public class MiddleOfLinkedList {
 
-	private static final int NUMERO_ELEMENTI = 10;
+    private static final int NUMERO_ELEMENTI = 10;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {		
-		for (int i = 1; i < NUMERO_ELEMENTI; i++) {
-			// Crea la lista della lunghezza richiesta
-			LinkedList<Object> lista = setupList(i);
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        for (int i = 1; i < NUMERO_ELEMENTI; i++) {
+            // Crea la lista della lunghezza richiesta
+            LinkedList<Object> lista = setupList(i);
 
-			// Trova l'elemento centrale
-			Object mezzo = findMiddle(lista);
+            // Trova l'elemento centrale
+            Object mezzo = findMiddle(lista);
 
-			// Stampa risultato
-			System.out.print("Numero elementi: " + i);
-			System.out.println(" - Elemento intermedio: " + mezzo.toString());
-			// System.out.println("");
+            // Stampa risultato
+            System.out.print("Numero elementi: " + i);
+            System.out.println(" - Elemento intermedio: " + mezzo.toString());
+            // System.out.println("");
 
-		}
-		
-	}
-	
-	private static LinkedList<Object> setupList(int n) {
-		LinkedList<Object> lista = new LinkedList<Object>();
-		for (int i = 1; i <= n; i++) {
-			lista.add(Integer.valueOf(i));
-		}
-		return lista;
-	}
+        }
 
-	/**
-	 * Cerca e ritorna l'elemento di mezzo della lista.
-	 * Se la lista non ha elementi ritorna null.
-	 * @param lista
-	 * @return
-	 */
-	public static Object findMiddle(LinkedList<Object> lista) {
-		Object inMezzo = null;
-		int contatore = 1;
-		Iterator<Object> i = lista.iterator();
-		Iterator<Object> dispari = lista.iterator();
-		
-		while (i.hasNext()) {
-			i.next();
-			if ( isOdd(contatore) ) {
-				inMezzo = dispari.next();;
-			}			
-			contatore++;
-		}
-		return inMezzo;
-	}
+    }
 
-	static boolean isOdd(int p) {
-		return (p % 2) == 1;
-	}
+    private static LinkedList<Object> setupList(int n) {
+        LinkedList<Object> lista = new LinkedList<Object>();
+        for (int i = 1; i <= n; i++) {
+            lista.add(Integer.valueOf(i));
+        }
+        return lista;
+    }
+
+    /**
+     * Cerca e ritorna l'elemento di mezzo della lista. Se la lista non ha elementi ritorna null.
+     * 
+     * @param lista
+     * @return
+     */
+    public static Object findMiddle(LinkedList<Object> lista) {
+        Object inMezzo = null;
+        int contatore = 1;
+        Iterator<Object> i = lista.iterator();
+        Iterator<Object> dispari = lista.iterator();
+
+        while (i.hasNext()) {
+            i.next();
+            if (isOdd(contatore)) {
+                inMezzo = dispari.next();
+                ;
+            }
+            contatore++;
+        }
+        return inMezzo;
+    }
+
+    static boolean isOdd(int p) {
+        return (p % 2) == 1;
+    }
 }
--- src/test/java/com/robertozagni/algoritmi/list/MiddleOfLinkedListTest.java
+++ src/test/java/com/robertozagni/algoritmi/list/MiddleOfLinkedListTest.java
@@ -1,78 +1,86 @@
+/**
+ * Copyright (c) 2014, 2016 Roberto Zagni, www.robertozagni.com
+ * The contents of this repository  are released under Apache 2.0 License unless stated differently; 
+ * see LICENSE file for complete text. 
+ * 
+ * @author roberto.zagni
+ */
 package com.robertozagni.algoritmi.list;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.LinkedList;
 
 import org.junit.Test;
 
-import com.robertozagni.algoritmi.list.MiddleOfLinkedList;
-
 public class MiddleOfLinkedListTest {
 
-	@Test
-	public void theMiddleOfAnEmptyListIsNull() {
-		LinkedList<Object> emptyList = new LinkedList<Object>();
-		
-		Object actual = MiddleOfLinkedList.findMiddle(emptyList);
-		
-		assertNull(actual);
-	}
+    @Test
+    public void theMiddleOfAnEmptyListIsNull() {
+        LinkedList<Object> emptyList = new LinkedList<Object>();
 
-	@Test
-	public void theMiddleOfAnListContainingJustAnItemIsTheItemItself() {
-		LinkedList<Object> list = new LinkedList<Object>();
-		Object anObject = new Object();
-		list.add(anObject);
-		
-		Object actual = MiddleOfLinkedList.findMiddle(list);
-		
-		assertEquals(anObject, actual);
-	}
-	
-	@Test
-	public void getsTheMiddleOfAList() {
-		Object middle = new Object();
-		LinkedList<Object> list = new LinkedList<Object>();
+        Object actual = MiddleOfLinkedList.findMiddle(emptyList);
 
-		list.add(new Object());
-		list.add(new Object());
-		list.add(middle );
-		list.add(new Object());
-		list.add(new Object());
-		
-		Object actual = MiddleOfLinkedList.findMiddle(list);
-		
-		assertEquals(middle, actual);
-	}
-	
-	@Test
-	public void getsTheMiddleOfAListWithAnOddNumberOfItems() {
-		Object middle = new Object();
-		LinkedList<Object> list = new LinkedList<Object>();
+        assertNull(actual);
+    }
 
-		list.add(new Object());
-		list.add(middle );
-		list.add(new Object());
-		list.add(new Object());
-		
-		Object actual = MiddleOfLinkedList.findMiddle(list);
-		
-		assertEquals(middle, actual);
-	}
-	
-	@Test
-	public void isOddIsTrueForOddNumbers() {
-		boolean actual = MiddleOfLinkedList.isOdd(101);
-		
-		assertTrue(actual);
-	}
-	
-	@Test
-	public void isOddIsFalseForEvenNumbers() {
-		boolean actual = MiddleOfLinkedList.isOdd(12);
-		
-		assertFalse(actual);
-	}
-	
+    @Test
+    public void theMiddleOfAnListContainingJustAnItemIsTheItemItself() {
+        LinkedList<Object> list = new LinkedList<Object>();
+        Object anObject = new Object();
+        list.add(anObject);
+
+        Object actual = MiddleOfLinkedList.findMiddle(list);
+
+        assertEquals(anObject, actual);
+    }
+
+    @Test
+    public void getsTheMiddleOfAList() {
+        Object middle = new Object();
+        LinkedList<Object> list = new LinkedList<Object>();
+
+        list.add(new Object());
+        list.add(new Object());
+        list.add(middle);
+        list.add(new Object());
+        list.add(new Object());
+
+        Object actual = MiddleOfLinkedList.findMiddle(list);
+
+        assertEquals(middle, actual);
+    }
+
+    @Test
+    public void getsTheMiddleOfAListWithAnOddNumberOfItems() {
+        Object middle = new Object();
+        LinkedList<Object> list = new LinkedList<Object>();
+
+        list.add(new Object());
+        list.add(middle);
+        list.add(new Object());
+        list.add(new Object());
+
+        Object actual = MiddleOfLinkedList.findMiddle(list);
+
+        assertEquals(middle, actual);
+    }
+
+    @Test
+    public void isOddIsTrueForOddNumbers() {
+        boolean actual = MiddleOfLinkedList.isOdd(101);
+
+        assertTrue(actual);
+    }
+
+    @Test
+    public void isOddIsFalseForEvenNumbers() {
+        boolean actual = MiddleOfLinkedList.isOdd(12);
+
+        assertFalse(actual);
+    }
+
 }
