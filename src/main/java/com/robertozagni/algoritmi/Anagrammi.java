fe045e5f2c3f6c30a495004fb2e0a19dbedfac5d
Roberto Zagni <rz70rz70@gmail.com>
2016-01-17 18:29:30 +0200

    Finds all the words with the same anagram from a text file.

--- src/main/java/com/robertozagni/algoritmi/Anagrammi.java
+++ src/main/java/com/robertozagni/algoritmi/Anagrammi.java
@@ -1,8 +1,14 @@
+/**
+ * Copyright (c) 2014, 2016 Roberto Zagni, www.robertozagni.com
+ * The contents of this repository  are released under Apache 2.0 License unless stated differently; 
+ * see LICENSE file for complete text. 
+ * 
+ * @author roberto.zagni
+ */
 package com.robertozagni.algoritmi;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -13,100 +19,125 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+/**
+ * Finds all the words with the same anagram from a text file.
+ * 
+ * @author roberto.zagni
+ */
 public class Anagrammi {
 
-	private static final String FILE_NAME = "dictionary.txt";
-	private static final int MIN_GROUP_SIZE = 8;
-	
-	/**
-	 * Costruisce gli anagrammi delle parole di un dizionario in formato testo.
-	 * @param args Il primo parametro � il nome del file con le parole.
-	 */
-	public static void main(String[] args) {
+    private static final String FILE_NAME = "dictionary.txt";
+    private static final int MIN_GROUP_SIZE = 8;
 
-		String fileName = FILE_NAME;
-		if (args.length > 0)
-			fileName = args[0];
-		File file = new File(fileName);
-		
-		// Carica le parole in liste di anagrammi
-		Map<String, List<String>> m = readWords(file);
-		System.out.println("*** Lette " + m.size() +" chiavi diverse.");
-		
-		// Estrai le liste con almeno MIN_GROUP_SIZE elementi
-		List<List<String>> winners = selectLong(m.values(), MIN_GROUP_SIZE);
-		System.out.println("*** Lette " + winners.size() +" collezioni di almeno " + MIN_GROUP_SIZE + " elementi.");
-		
-		// Crea un comparator che ordina in base a size() della lista
-		Comparator<List<String>> c = new Comparator<List<String>>(){
-			/* (non-Javadoc)
-			 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
-			 */
-			@Override
-			public int compare(List<String> arg0, List<String> arg1) {
-				return (arg1.size() - arg0.size());
-			}			
-		};	// FINE di Comparator
-		// Ordina in base alla dimensione della lista
-		Collections.sort(winners, c);
-		
-		// Stampa risultato
-		for (List<String> l : winners){
-			System.out.println("* (" + l.size() +") =>" + l);
-		}
-		
-	} // FINE di MAIN
+    /**
+     * Costruisce gli anagrammi delle parole di un dizionario in formato testo.
+     * 
+     * @param args Il primo parametro è il nome del file con le parole.
+     */
+    public static void main(String[] args) {
 
-	public static Map<String, List<String>> readWords(File f) {
-		Map<String, List<String>> m = new HashMap<String, List<String>>();
-		Scanner s = null;
-		try {			
-			s = new Scanner(f);
-			while (s.hasNext()){
-				String word = s.next();	// Legge prox parola
-				String alpha = alphabetize(word);	// ordina le lettere in modo alfabetico
-				List<String> l = m.get(alpha);	// Cerca la lista corrispondente
-				if (l == null) {
-					l = new ArrayList<String>();
-					m.put(alpha, l);
-				}
-				l.add(word);
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (NullPointerException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			System.err.println(e);
+        String fileName = FILE_NAME;
+        if (args.length > 0)
+            fileName = args[0];
+        File file = new File(fileName);
+
+        Map<String, List<String>> m = readWords(file);
+        System.out.println("*** Lette " + m.size() + " chiavi diverse.");
+
+        List<List<String>> winners = selectLong(m.values(), MIN_GROUP_SIZE);
+        System.out.println("*** Lette " + winners.size() + " collezioni di almeno " + MIN_GROUP_SIZE + " elementi.");
+
+        Comparator<List<String>> c = new Comparator<List<String>>() {
+            @Override
+            public int compare(List<String> arg0, List<String> arg1) {
+                return (arg1.size() - arg0.size());
+            }
+        };
+        Collections.sort(winners, c);
+
+        for (List<String> l : winners) {
+            System.out.println("* (" + l.size() + ") =>" + l);
+        }
+
+    }
+
+    /**
+     * Scans with the given file and returns a map associating all the groups of alphabetically sorted letters with a
+     * list of the words of the file that produce that sorted group.
+     * 
+     * @param f the file to open and read.
+     * @return the map associating groups of sorted letters with the corresponding words.
+     */
+    public static Map<String, List<String>> readWords(File f) {
+        Map<String, List<String>> m = new HashMap<String, List<String>>();
+        Scanner s = null;
+        try {
+            s = new Scanner(f);
+            while (s.hasNext()) {
+                String word = s.next();
+                String alpha = sortLetters(word);
+                List<String> l = getWordsList(m, alpha);
+                l.add(word);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println(e);
             System.exit(1);
-		} finally {
-			if ( s != null) {
-				s.close();
-			}
-		}
-		return m;
-	}
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }
+        return m;
+    }
 
-	/**
-	 * Order alphabetically the characters in the passed String
-	 * @param word	The string to order. If it contains spaces they will be at the start of the ordered result.
-	 * @return The passed string's letters ordered alphabetically 
-	 */
-	protected static String alphabetize(String word) {
+    /**
+     * Gets or creates the list holding the words corresponding to a string of alphabetically ordered letters.<br>
+     * Newly created lists are inserted into the map with the given group of letters as key.
+     * 
+     * @param m the map holding the list for every group of letters
+     * @param alpha the group of letters we want the corresponding list of words
+     * @return the list of words associated to the given the group of letters
+     */
+    private static List<String> getWordsList(Map<String, List<String>> m, String alpha) {
+        List<String> l = m.get(alpha);
+        if (l == null) {
+            l = new ArrayList<String>();
+            m.put(alpha, l);
+        }
+        return l;
+    }
 
-		char[] chars = word.toCharArray();
-		Arrays.sort(chars); 
-		return new String(chars);
-	}
+    /**
+     * Order alphabetically the characters in the passed String
+     * 
+     * @param word The string to order. If it contains spaces they will be at the start of the ordered result.
+     * @return The passed string's letters ordered alphabetically
+     */
+    protected static String sortLetters(String word) {
 
-	// Estrai le liste con almeno MIN_GROUP_SIZE elementi
-	public static List<List<String>> selectLong(Collection<List<String>> values, int minSize) {
-		List<List<String>> winners = new ArrayList<List<String>>();
-		for (List<String> list : values) {
-			if (list.size() >= minSize)
-				winners.add(list);
-		}
-		return winners;
-	}
+        char[] chars = word.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+
+    /**
+     * Selects from the given collection the lists with at least the required number of elements.
+     * 
+     * @param values the collection holding the lists.
+     * @param minSize the minimum number of elements a list must have to be selected.
+     * @return A list of the selected lists.
+     */
+    public static <T> List<List<T>> selectLong(Collection<List<T>> values, int minSize) {
+        List<List<T>> winners = new ArrayList<List<T>>();
+        for (List<T> list : values) {
+            if (list.size() >= minSize)
+                winners.add(list);
+        }
+        return winners;
+    }
 
 }
--- src/test/java/com/robertozagni/algoritmi/AnagrammiTest.java
+++ src/test/java/com/robertozagni/algoritmi/AnagrammiTest.java
@@ -1,14 +1,22 @@
+/**
+ * Copyright (c) 2014, 2016 Roberto Zagni, www.robertozagni.com
+ * The contents of this repository  are released under Apache 2.0 License unless stated differently; 
+ * see LICENSE file for complete text. 
+ * 
+ * @author roberto.zagni
+ */
 package com.robertozagni.algoritmi;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -17,146 +25,144 @@ import java.util.Map;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
-
-import com.robertozagni.algoritmi.Anagrammi;
 
 public class AnagrammiTest {
 
-	public AnagrammiTest() {
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @Test
+    public void shouldReturnEmptyMapForNullFile() {
+        Map<String, List<String>> m = Anagrammi.readWords(null);
 
-	@Test
-	public void shouldReturnEmptyMapForNullFile() {
-		Map<String, List<String>> m = Anagrammi.readWords(null);
-		
-		assertNotNull(m);
-		assertTrue(m.isEmpty());
-		assertEquals(0, m.size());
-	}
-	
-	@Test 
-	public void shouldReturnEmptyMapForEmptyFile() {
-		Map<String, List<String>> m = Anagrammi.readWords(new File(""));
-		
-		assertNotNull(m);
-		assertTrue(m.isEmpty());
-		assertEquals(0, m.size());
-		
-	}
+        assertNotNull(m);
+        assertTrue(m.isEmpty());
+        assertEquals(0, m.size());
+    }
 
-	@Test
-	public void shouldReturnEmptyMapForNotExistingFile() {
-		Map<String, List<String>> m = Anagrammi.readWords(new File("ANotExistingFile.boh"));
-		
-		assertNotNull(m);
-		assertTrue(m.isEmpty());
-		assertEquals(0, m.size());
-		
-	}
+    @Test
+    public void shouldReturnEmptyMapForEmptyFile() {
+        Map<String, List<String>> m = Anagrammi.readWords(new File(""));
 
-	@Test
-	public void shouldReadEmptyFiles() {
-		int NUM_WORDS = 0;
-		File f = createTextFileWithNWords(NUM_WORDS);
-		Map<String, List<String>> m = Anagrammi.readWords(f);
-		f.delete();
-		
-		assertNotNull(m);
-		assertTrue(m.isEmpty());
-		assertEquals(NUM_WORDS, m.size());
-		
-	}
+        assertNotNull(m);
+        assertTrue(m.isEmpty());
+        assertEquals(0, m.size());
 
-	@Test
-	public void shouldReadFromNotEmptyFiles() {
-		int NUM_WORDS = 3;
-		File f = createTextFileWithNWords(NUM_WORDS);
-		Map<String, List<String>> m = Anagrammi.readWords(f);
-		f.delete();
-		
-		assertNotNull(m);
-		assertFalse(m.isEmpty());
-		assertEquals(NUM_WORDS, m.size());
-		
-		assertTrue(m.containsKey("0_dorw"));
-		assertTrue(m.get("0_dorw").contains("word_0") );
-		
-		assertTrue(m.containsKey("2_dorw"));
-		assertTrue(m.get("2_dorw").contains("word_2") );
-		
-	}
+    }
 
-	/*
-	 *  Ancillary method to Tests to create a file with 3 words in it. 
-	 */
-	private File createTextFileWithNWords(int numberOfWords) {
-		File f = null;
-		try {
-			f = File.createTempFile("Anagrammi", null);
-			Writer writer = new BufferedWriter(
-								new OutputStreamWriter(
-										new FileOutputStream(f), "utf-8"));
-			for (int i = 0; i < numberOfWords; i++) {
-				writer.write("word_" + i + "\n");
-			}
-			writer.flush();
-			writer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return f;
-	}
+    @Test
+    public void shouldReturnEmptyMapForNotExistingFile() {
+        Map<String, List<String>> m = Anagrammi.readWords(new File("ANotExistingFile.boh"));
 
-	@Test(expected = java.lang.NullPointerException.class)
-	public void alfabetizeShouldThrowExceptionForNullString() {
-		String s = Anagrammi.alphabetize(null);
-	}
-	
-	@Test
-	public void alfabetizeShouldWorkForEmptyString() {
-		String s = Anagrammi.alphabetize("");
-		assertNotNull(s);
-		assertEquals("", s);
-	}
+        assertNotNull(m);
+        assertTrue(m.isEmpty());
+        assertEquals(0, m.size());
 
-	@Test
-	public void alfabetizeShouldWorkForRegularString() {
-		String s = Anagrammi.alphabetize("ciao");
-		assertNotNull(s);
-		assertEquals("acio", s);
-	}
+    }
 
-	@Test
-	public void alfabetizeShouldWorkForRegularStringWithSpaces() {
-		String s = Anagrammi.alphabetize("Ella e laila");
-		assertNotNull(s);
-		assertEquals("  Eaaaeillll", s);
-	}
+    @Test
+    public void shouldReadEmptyFiles() {
+        int NUM_WORDS = 0;
+        File f = createTextFileWithNWords(NUM_WORDS);
+        Map<String, List<String>> m = Anagrammi.readWords(f);
+        f.delete();
 
-	@Test
-	public void testSelectLong() {
-		List<String> list1 = new ArrayList<String>(); list1.add("uno");
-		List<String> list4 = new ArrayList<String>(); 
-			list4.add("uno"); list4.add("due"); list4.add("tre"); list4.add("quattro");
-		HashSet<List<String>> values = new HashSet<List<String>>();
-			values.add(list1); values.add(list4);
-		
-		List<List<String>> l = Anagrammi.selectLong(values, 2);
-		
-		assertEquals(1, l.size());
-		assertEquals(4, l.get(0).size());
-		
-		
-	}
+        assertNotNull(m);
+        assertTrue(m.isEmpty());
+        assertEquals(NUM_WORDS, m.size());
+
+    }
+
+    @Test
+    public void shouldReadFromNotEmptyFiles() {
+        int NUM_WORDS = 3;
+        File f = createTextFileWithNWords(NUM_WORDS);
+        Map<String, List<String>> m = Anagrammi.readWords(f);
+        f.delete();
+
+        assertNotNull(m);
+        assertFalse(m.isEmpty());
+        assertEquals(NUM_WORDS, m.size());
+
+        assertTrue(m.containsKey("0_dorw"));
+        assertTrue(m.get("0_dorw").contains("word_0"));
+
+        assertTrue(m.containsKey("2_dorw"));
+        assertTrue(m.get("2_dorw").contains("word_2"));
+
+    }
+
+    /*
+     * Ancillary method to Tests to create a file with N words in it.
+     */
+    private File createTextFileWithNWords(int numberOfWords) {
+        File f = null;
+        try {
+            f = File.createTempFile("Anagrammi", null);
+            Writer writer = new BufferedWriter(
+                    new OutputStreamWriter(
+                            new FileOutputStream(f), "utf-8"));
+            for (int i = 0; i < numberOfWords; i++) {
+                writer.write("word_" + i + "\n");
+            }
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return f;
+    }
+
+    @Test(expected = java.lang.NullPointerException.class)
+    public void alfabetizeShouldThrowExceptionForNullString() {
+        Anagrammi.sortLetters(null);
+    }
+
+    @Test
+    public void alfabetizeShouldWorkForEmptyString() {
+        String s = Anagrammi.sortLetters("");
+        assertNotNull(s);
+        assertEquals("", s);
+    }
+
+    @Test
+    public void alfabetizeShouldWorkForRegularString() {
+        String s = Anagrammi.sortLetters("ciao");
+        assertNotNull(s);
+        assertEquals("acio", s);
+    }
+
+    @Test
+    public void alfabetizeShouldWorkForRegularStringWithSpaces() {
+        String s = Anagrammi.sortLetters("Ella e laila");
+        assertNotNull(s);
+        assertEquals("  Eaaaeillll", s);
+    }
+
+    @Test
+    public void testSelectLong() {
+        List<String> list1 = new ArrayList<String>();
+        list1.add("uno");
+        List<String> list4 = new ArrayList<String>();
+        list4.add("uno");
+        list4.add("due");
+        list4.add("tre");
+        list4.add("quattro");
+        HashSet<List<String>> values = new HashSet<List<String>>();
+        values.add(list1);
+        values.add(list4);
+
+        List<List<String>> l = Anagrammi.selectLong(values, 2);
+
+        assertEquals(1, l.size());
+        assertEquals(4, l.get(0).size());
+
+    }
 
 }
