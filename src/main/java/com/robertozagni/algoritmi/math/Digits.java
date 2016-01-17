41281234f5dc1a447e2501d4bdee57b5424e1d64
Roberto Zagni <rz70rz70@gmail.com>
2016-01-17 11:02:09 +0200

    Converts between number and array of digits representation.
    The conversion works for all long values and is symmetric, so you can go
    back and forth between the two representations.
    If the digit array contains multiple digit values or the values have
    mixed sign the resulting value is the base 10 positional weighted sum.

---
+++ src/main/java/com/robertozagni/algoritmi/math/Digits.java
@@ -0,0 +1,76 @@
+package com.robertozagni.algoritmi.math;
+
+/**
+ * 
+ * @author roberto.zagni
+ *
+ */
+public class Digits {
+
+    /**
+     * Breaks up the provided number into an array with a single digit integer at every position.<br>
+     * If the given number is negative all the digits in the returned array are negative.
+     * 
+     * @param number The number to break up.
+     * @return an array with the digits of the number, one at every position of the array
+     */
+    public static int[] breakIntoDigits(long number) {
+
+        // Symmetric positive value is out of range => - Long.MIN_VALUE == Long.MIN_VALUE !!
+        if (number == Long.MIN_VALUE) { // -9223372036854775808, just return its representation.
+            return new int[] { -9, -2, -2, -3, -3, -7, -2, -0, -3, -6, -8, -5, -4, -7, -7, -5, -8, -0, -8 };
+        }
+        if (number <= 9 && number >= -9) {
+            return new int[] { (int) number };
+        }
+
+        int sgn = 1;
+        if (number < 0) {
+            sgn = -1;
+        }
+
+        int length = (int) Math.floor(Math.log10(number * sgn)) + 1;
+        int[] digits = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            digits[length - 1 - i] = (int) (number % 10);
+            number = number / 10;
+        }
+        return digits;
+    }
+
+    /**
+     * Joins the digits from the given positive single digit values array.<br>
+     * The result is the number obtained by joining the digits from the array if the numbers in the array are single
+     * digit numbers all with the same sign.<br>
+     * If numbers in the array are not same sign single digit numbers the returned value is the base 10 weighted sum of
+     * the values, instead of the simple joining of the digits.
+     * 
+     * @param digits the array with the digits to join, lower indices have higher positional weighting.
+     * @return the number obtained by joining the digits if same sign single digit array is passed or the base 10
+     *         positional weighted sum of the values from the array.
+     */
+    public static long joinDigits(int[] digits) {
+        long number = 0;
+
+        if (digits != null && digits.length > 0) {
+            // int sgn = 1;
+            // if (digits[0] < 0) {
+            // sgn = -1;
+            // digits[0] = digits[0] * sgn;
+            // }
+
+            long power = 1;
+            for (int i = digits.length - 1; i >= 0; i--) {
+                number = number + digits[i] * power;
+                power *= 10;
+            }
+
+            // number = number * sgn;
+
+        }
+
+        return number;
+    }
+
+}
---
+++ src/test/java/com/robertozagni/algoritmi/math/DigitsTest.java
@@ -0,0 +1,86 @@
+package com.robertozagni.algoritmi.math;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DigitsTest {
+
+    @Test
+    public void canBreakUpNumberIntoDigits() {
+        long number = 123456;
+        int[] digitsExpected = { 1, 2, 3, 4, 5, 6 };
+
+        int[] digitsActual = Digits.breakIntoDigits(number);
+        assertEquals(digitsExpected.length, digitsActual.length);
+        assertArrayEquals(digitsExpected, digitsActual);
+
+        assertArrayEquals(new int[] { 0 }, Digits.breakIntoDigits(0));
+        assertArrayEquals(new int[] { 1 }, Digits.breakIntoDigits(1));
+        assertArrayEquals(new int[] { 9, 9, 9 }, Digits.breakIntoDigits(999));
+        assertArrayEquals(new int[] { 1, 0, 0, 0 }, Digits.breakIntoDigits(1000));
+        assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, Digits.breakIntoDigits(1234567890L));
+
+        assertArrayEquals(new int[] { -1 }, Digits.breakIntoDigits(-1));
+        assertArrayEquals(new int[] { -9, -9, -9 }, Digits.breakIntoDigits(-999));
+        assertArrayEquals(new int[] { -1, -2, -3, -4, -5, -6, -7, -8, -9, -0 }, Digits.breakIntoDigits(-1234567890L));
+    }
+
+    @Test
+    public void canBreakUpMaxAndMinLong() throws Exception {
+        assertArrayEquals(new int[] { 9, 2, 2, 3, 3, 7, 2, 0, 3, 6, 8, 5, 4, 7, 7, 5, 8, 0, 7 },
+                Digits.breakIntoDigits(Long.MAX_VALUE)); // 9223372036854775807
+        assertArrayEquals(new int[] { -9, -2, -2, -3, -3, -7, -2, -0, -3, -6, -8, -5, -4, -7, -7, -5, -8, -0, -8 },
+                Digits.breakIntoDigits(Long.MIN_VALUE)); // -9223372036854775808
+
+    }
+
+    @Test
+    public void canJoinBackMaxAndMinLong() throws Exception {
+        assertEquals(Long.MAX_VALUE, // 9223372036854775807
+                Digits.joinDigits(new int[] { 9, 2, 2, 3, 3, 7, 2, 0, 3, 6, 8, 5, 4, 7, 7, 5, 8, 0, 7 }));
+        assertEquals(Long.MIN_VALUE, Digits // -9223372036854775808
+                .joinDigits(new int[] { -9, -2, -2, -3, -3, -7, -2, -0, -3, -6, -8, -5, -4, -7, -7, -5, -8, -0, -8 }));
+
+    }
+
+    @Test
+    public void canJoinUpSameSignSingleDigitsArrays() throws Exception {
+        int[] digits = { 1, 2, 3, 4, 5, 6 };
+        long numberExpected = 123456;
+
+        long numberActual = Digits.joinDigits(digits);
+        assertEquals(numberExpected, numberActual);
+
+        assertEquals(0, Digits.joinDigits(new int[] {}));
+        assertEquals(0, Digits.joinDigits(new int[] { 0 }));
+        assertEquals(90 + 9, Digits.joinDigits(new int[] { 9, 9 }));
+        assertEquals(1000, Digits.joinDigits(new int[] { 1, 0, 0, 0 }));
+
+        assertEquals(-1, Digits.joinDigits(new int[] { -1 }));
+        assertEquals(-99, Digits.joinDigits(new int[] { -9, -9 }));
+        assertEquals(-1000, Digits.joinDigits(new int[] { -1, 0, 0, 0 }));
+
+        // Positional weights keep working for multi digit values into array
+        assertEquals(24, Digits.joinDigits(new int[] { 1, 14 }));
+        assertEquals(1024, Digits.joinDigits(new int[] { 1, 1014 }));
+    }
+
+    @Test
+    public void returnZeroOnJoinNullOrEmptyArray() throws Exception {
+        assertEquals(0, Digits.joinDigits(null));
+        assertEquals(0, Digits.joinDigits(new int[] {}));
+    }
+
+    @Test
+    public void positionalWeightingWithMixedSignsInTheArray() throws Exception {
+
+        assertEquals(90 - 9, Digits.joinDigits(new int[] { 9, -9 }));
+        assertEquals(9000 - 900 - 20 + 3, Digits.joinDigits(new int[] { 9, -9, -2, 3 }));
+        assertEquals(-9000 - 900 - 20 + 3, Digits.joinDigits(new int[] { -9, -9, -2, 3 }));
+        assertEquals(-9000 + 0 + 50 - 3, Digits.joinDigits(new int[] { -9, 0, 5, -3 }));
+
+    }
+
+}
