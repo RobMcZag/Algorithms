f948f5bc79d1a038e4b205593bc1548840c92e9d
Roberto Zagni <rz70rz70@gmail.com>
2016-01-17 19:54:40 +0200

    Sources have license information.

--- src/main/java/com/robertozagni/algoritmi/binaryHeap/BinaryHeapException.java
+++ src/main/java/com/robertozagni/algoritmi/binaryHeap/BinaryHeapException.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2014, 2016 Roberto Zagni, www.robertozagni.com
+ * The contents of this repository  are released under Apache 2.0 License unless stated differently; 
+ * see LICENSE file for complete text. 
+ * 
+ * @author roberto.zagni
+ */
 package com.robertozagni.algoritmi.binaryHeap;
 
 public class BinaryHeapException extends RuntimeException {
