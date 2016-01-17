99423f22b7d2f22aed2258b3104e63dcb36677f7
Roberto Zagni <rz70rz70@gmail.com>
2016-01-17 13:33:35 +0200

    Added copyright notice.

---
+++ src/main/java/com/robertozagni/algoritmi/math/Multiplication.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2014, 2016 Roberto Zagni, www.robertozagni.com
+ * The contents of this repository  are released under Apache 2.0 License unless stated differently; 
+ * see LICENSE file for complete text. 
+ * 
+ * @author roberto.zagni
+ */
+package com.robertozagni.algoritmi.math;
+
+/**
+ * A class collecting different algorithms to perform multiplications and other related operations.
+ * 
+ * @author roberto.zagni
+ */
+public class Multiplication {
+
+}
---
+++ src/test/java/com/robertozagni/algoritmi/math/MultiplicationTest.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2014, 2016 Roberto Zagni, www.robertozagni.com
+ * The contents of this repository  are released under Apache 2.0 License unless stated differently; 
+ * see LICENSE file for complete text. 
+ * 
+ * @author roberto.zagni
+ */
+package com.robertozagni.algoritmi.math;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class MultiplicationTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
