090e0924812155a3797aadce6479a23eb6ab506b
Roberto Zagni <rz70rz70@gmail.com>
2016-02-04 20:10:40 +0200

    Randomized queue implemented as a resizing array.

--- src/main/java/com/robertozagni/algoritmi/list/RandomizedQueue.java
+++ src/main/java/com/robertozagni/algoritmi/list/RandomizedQueue.java
@@ -1,15 +1,50 @@
 package com.robertozagni.algoritmi.list;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class RandomizedQueue<E> {
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomizedQueue<E> implements Iterable<E> {
+
+  private static class RandomizedQueueIterator<E> implements Iterator<E> {
+
+    private int pos = 0;
+    private final int N;
+    private final E[] itvals;
+
+    @SuppressWarnings("unchecked")
+    RandomizedQueueIterator(E[] v, int n) {
+      N = n;
+      itvals = (E[]) new Object[N];
+      System.arraycopy(v, 0, itvals, 0, N);
+      StdRandom.shuffle(itvals);
+    }
+
+    @Override
+    public boolean hasNext() {
+      return pos < N;
+    }
+
+    @Override
+    public E next() {
+      if (pos >= N) {
+        throw new NoSuchElementException("There are no more elements in this iterator.");
+      }
+      return itvals[pos++];
+    }
+
+    @Override
+    public void remove() {
+      throw new UnsupportedOperationException("The remove operation is not supported by this iterator.");
+    }
+
+  }
 
   private static final int MIN_SIZE = 8;
 
   private E[] vals = null;
 
-  private int first = MIN_SIZE / 2;
-  private int last = first;
   private int count = 0;
 
   /**
@@ -41,35 +76,56 @@ public class RandomizedQueue<E> {
    * @param item the item to be added.
    */
   public void enqueue(E item) {
-    // TODO - Implement
-    count++;
+    if (item == null) {
+      throw new NullPointerException("This queue does not support null values.");
+    }
+    vals[count++] = item;
+    if (count == vals.length) {
+      resize(count * 2);
+    }
+  }
+
+  private void resize(int n) {
+    // System.out.format("Count is %4d => from %4d to %4d.%n", count, vals.length, n);
+    @SuppressWarnings("unchecked")
+    E[] dest = (E[]) new Object[n];
+    System.arraycopy(vals, 0, dest, 0, count);
+    vals = dest;
   }
 
   /**
    * Remove and return a random item.
    */
   public E dequeue() {
-    // TODO - Implement
-    count--;
-    return null;
+    if (isEmpty()) {
+      throw new NoSuchElementException("This queue is empty.");
+    }
+    int pos = StdRandom.uniform(count--);
+    E val = vals[pos];
+    vals[pos] = vals[count];
+    vals[count] = null;
+    if (count >= MIN_SIZE / 2 && count == vals.length / 4) {
+      resize(count * 2);
+    }
+    return val;
   }
 
   /**
    * Return (but do not remove) a random item.
    */
   public E sample() {
-    // TODO - Implement
-
-    return null;
+    if (isEmpty()) {
+      throw new NoSuchElementException("This queue is empty.");
+    }
+    int pos = StdRandom.uniform(count);
+    return vals[pos];
   }
 
   /**
    * Return an independent iterator over items in random order.
    */
   public Iterator<E> iterator() {
-    // TODO - Implement
-
-    return null;
+    return new RandomizedQueueIterator<E>(vals, count);
   }
 
   /**
